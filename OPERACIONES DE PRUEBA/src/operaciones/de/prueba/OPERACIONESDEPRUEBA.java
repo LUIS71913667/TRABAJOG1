a227639f16137feee9fef891d382186bae5a321a
luis71913667 <1427092@senati.pe>
2023-08-18 18:19:14 -0500

    trabajo grupal

---
+++ OPERACIONES DE PRUEBA/src/operaciones/de/prueba/OPERACIONESDEPRUEBA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones.de.prueba;
+
+/**
+ *
+ * @author Alumno
+ */
+public class OPERACIONESDEPRUEBA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
